14cd255caa414399603e7434a20dca043ec47bc5
GitHub Action <action@github.com>
2022-07-08 08:33:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/qzchenwl/demoplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/qzchenwl/demoplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.qzchenwl.demoplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qzchenwl.demoplugin.MyBundle
 
 class MyApplicationService {
 
